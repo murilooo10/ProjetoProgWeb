c12c85874bc69699ad2a3473608d940eba05f40a
Bruno Conke <brunoconke@gmail.com>
2018-09-11 23:00:57 -0300

    php login

--- PHP/index.php
+++ PHP/index.php
@@ -1,10 +1,26 @@
 <?php 
 
+$senha = $_POST['senha'];
+$email = $_POST['email'];
 
-	$xml_string = file_get_contents("../XML/Teste.xml");
+	$xml_string = file_get_contents("../XML/teste.xml");
 
 	$xml_objeto = simplexml_load_string($xml_string);
 
-	print_r($xml_objeto);
+	echo json_decode($xml_objeto->Users->Login);
+
+	if ($email == $xml_objeto->Users->Login && $senha == $xml_objeto->Users->Senha )
+	{
+
+		echo json_encode("valido"); 
+	}
+	else
+	{
+		echo json_encode("invalido"); 
+
+	}
+
+	
+	
 
  ?>
